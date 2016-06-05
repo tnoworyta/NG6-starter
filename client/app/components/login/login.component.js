e6d530758f7826f039dfe8e8f39e7833b8f5ebe4
tnoworyta <tomasz.noworyta@gmail.com>
2016-06-05 14:34:59 +0200

    Simple authentication added

--- client/app/components/login/login.component.js
+++ client/app/components/login/login.component.js
@@ -1,13 +1,15 @@
 import template from './login.html';
 import controller from './login.controller';
 import './login.styl';
+import 'firebase';
 
 let loginComponent = {
   restrict: 'E',
   bindings: {},
   template,
   controller,
-  controllerAs: 'vm'
+  controllerAs: 'vm',
+  providers: ['$firebaseAuth']
 };
 
 export default loginComponent;
--- client/app/components/login/login.controller.js
+++ client/app/components/login/login.controller.js
@@ -1,12 +1,25 @@
 class LoginController {
-  constructor() {
+  constructor($firebaseAuth) {
+    "ngInject";
     this.name = 'login';
-    //this.firebaseObj = new Firebase("https://mdoc-e8b69.firebaseio.com");
+    this.firebaseObj = new Firebase("https://mdoc1.firebaseio.com");
   }
 
   login() {
     console.log('Login called chief!')
     console.log(this.user)
+
+    this.firebaseObj.authWithPassword({
+      "email": this.user.email,
+      "password": this.user.password
+    }, (error, authData) => {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+      }
+    });
   }
 }
+
 export default LoginController;
