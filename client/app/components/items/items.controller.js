e2ca626fe21d0549d5cefb5b63a7f19fb8fae628
tnoworyta <tomasz.noworyta@gmail.com>
2016-06-26 12:09:20 +0200

    Items cleanup

--- client/app/components/items/items.controller.js
+++ client/app/components/items/items.controller.js
@@ -3,16 +3,12 @@ class ItemsController {
     "ngInject";
     this.name = 'items';
     this.ref = new Firebase("https://mdoc1.firebaseio.com/items");
-    this.itemRecords = $firebaseArray(this.ref);
+    this.items = $firebaseArray(this.ref);
   }
 
   add(item) {
-    this.itemRecords.$add({desc: item.desc});
+    this.items.$add({desc: item.desc});
     item.desc = undefined
-  }
-
-  remove(item){
-    this.itemRecords.$remove(item);
   }
 }
 
